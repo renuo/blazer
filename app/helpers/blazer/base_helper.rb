14d00bbb47aacd529289e7418028ac5be0af8d83
Liberatys <nick.flueckiger@renuo.ch>
2021-03-20 17:32:18 +0100

    Implement completion for table_names and columns

--- app/helpers/blazer/base_helper.rb
+++ app/helpers/blazer/base_helper.rb
@@ -11,6 +11,35 @@ module Blazer
     BLAZER_URL_REGEX = /\Ahttps?:\/\/[\S]+\z/
     BLAZER_IMAGE_EXT = %w[png jpg jpeg gif]
 
+    BLAZER_COMPLETION_COLUMN_SCORE = 1001
+    BLAZER_COMPLETION_COLUMN_META = 'column_name'
+    BLAZER_COMPLETION_TABLE_SCORE = 1000
+    BLAZER_COMPLETION_TABLE_META = 'table_name'
+
+
+    def extract_tables_and_columns(schema)
+      schema.map do |schema_entry|
+        [schema_entry[:table], schema_entry[:columns]]
+      end
+    end
+
+    def blazer_table_name_completion_source
+      extract_tables_and_columns(Blazer.data_sources['main'].schema).collect do |entry|
+        {
+          value: entry.first,
+          columns: entry.last.map do |column|
+            {
+              value: column[:name],
+              score: BLAZER_COMPLETION_COLUMN_SCORE,
+              meta: BLAZER_COMPLETION_COLUMN_META
+            }
+          end,
+          score: BLAZER_COMPLETION_TABLE_SCORE,
+          meta: BLAZER_COMPLETION_TABLE_META
+        }
+      end.to_json
+    end
+
     def blazer_format_value(key, value)
       if value.is_a?(Numeric) && !key.to_s.end_with?("id") && !key.to_s.start_with?("id")
         number_with_delimiter(value)
